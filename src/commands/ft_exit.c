1eb3132a8a1d80b96f3a9d7b9e2d6020d7a3b786
Radek Novotny <rnovotny@c3r7s1.42prague.com>
2024-10-12 13:18:00 +0200

    Fixed exit command, freeing mem on exit

--- src/commands/ft_exit.c
+++ src/commands/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 06:33:57 by lmaresov          #+#    #+#             */
-/*   Updated: 2024/09/22 10:03:53 by rnovotny         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:10:45 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@ void	ft_exit(t_ms *ms)
 {
 	char *exit_code;
 
-	if(!ms->tokens->next)
+	if (!(((t_cmd *)ms->commands->data)->arguments[0]))
 	{
 		// free memory and exit the minishell
+		free_all(ms);
 		exit(0);
 	}
-	if(ms->tokens->next->next)
+	if(((t_cmd *)ms->commands->data)->arguments[1])	// check argument list length
 	{
 		printf("exit: too many arguments\n");
 		return ;
 	}
-	exit_code = ms->tokens->next->data;
+	exit_code = ((t_cmd *)ms->commands->data)->arguments[0];
 	// if exit_code too long - undefined behaviour (same as bash)
 	// free memory and exit the minishell
+	free_all(ms);
 	exit(ft_atoi(exit_code));
 }
--- src/run_commands.c
+++ src/run_commands.c
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 09:11:11 by lmaresov          #+#    #+#             */
-/*   Updated: 2024/10/12 12:41:21 by rnovotny         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:04:09 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,13 +53,13 @@ int one_command(t_ms *ms)
         ft_cd(ms);
     else if (check_command(((t_cmd *)ms->commands->data)->command) == 3)
         ft_pwd(ms);
-    else if (check_command(ms->tokens->data) == 4)
+    else if (check_command(((t_cmd *)ms->commands->data)->command) == 4)
         ft_export(ms);
-    else if (check_command(ms->tokens->data) == 5)
+    else if (check_command(((t_cmd *)ms->commands->data)->command) == 5)
         ft_unset(ms);
     else if (check_command(((t_cmd *)ms->commands->data)->command) == 6)
         ft_env(ms);
-    else if (check_command(ms->tokens->data) == 7)
+    else if (check_command(((t_cmd *)ms->commands->data)->command) == 7)
         ft_exit(ms);
     return (1);   
 }
