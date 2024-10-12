1259a51bcd7b5730a8a19ff7998f3b7f93731c9d
Radek Novotny <rnovotny@c3r7s1.42prague.com>
2024-10-12 13:28:01 +0200

    Export works without arguments

--- src/commands/ft_export.c
+++ src/commands/ft_export.c
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 06:34:02 by lmaresov          #+#    #+#             */
-/*   Updated: 2024/10/12 12:21:34 by rnovotny         ###   ########.fr       */
+/*   Updated: 2024/10/12 13:26:45 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 
 void	ft_export(t_ms *ms)
 {
+	int		i;
 	t_listd	*envar;
 	t_env	*new_env;
 	t_listd	*new_node;
 
 	envar = ms->envar;
-	if (!ms->tokens->next)
+	if (!(((t_cmd *)ms->commands->data)->arguments[0]))
 	{
 		while (envar)
 		{
@@ -28,7 +29,8 @@ void	ft_export(t_ms *ms)
 		}
 		return ;
 	}
-	while (ms->tokens->next)
+	i = 0;
+	while (((t_cmd *)ms->commands->data)->arguments[i++])
 	{
 		new_env = malloc(sizeof(t_env));
 		new_env->env_key = ms->tokens->next->data;
