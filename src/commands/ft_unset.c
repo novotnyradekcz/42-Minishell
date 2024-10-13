86722574f5572fc27fc369e834f9205e07711abe
Radek Novotný <novotnyradekcz@gmail.com>
2024-10-13 10:25:43 +0200

    Finished unset

--- src/commands/ft_unset.c
+++ src/commands/ft_unset.c
@@ -6,50 +6,53 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 06:34:12 by lmaresov          #+#    #+#             */
-/*   Updated: 2024/10/12 11:20:08 by rnovotny         ###   ########.fr       */
+/*   Updated: 2024/10/13 10:23:42 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	rm_from_env(t_ms *ms, t_listd *envar, char *key)
+void	rm_from_env(t_ms *ms, char *key)
 {
-	t_listd	*tmp;
+	t_listd	*envar;
 	t_listd	*prev;
 
-	tmp = envar;
+	envar = ms->envar;
 	prev = NULL;
-	while (tmp)
+	while (envar)
 	{
-		if (ft_strcmp(((t_env *)tmp->data)->env_key, key))
+		if (!(ft_strcmp(((t_env *)envar->data)->env_key, key)))
 		{
 			if (prev)
-				prev->next = tmp->next;
+				prev->next = envar->next;
 			else
-				ms->envar = tmp->next;
-			free(tmp->data);
-			free(tmp);
+				ms->envar = envar->next;
+			free(((t_env *)envar->data)->env_key);
+			if (((t_env *)envar->data)->env_value[0])
+				free(((t_env *)envar->data)->env_value);
+			free(envar->data);
+			free(envar);
 			return ;
 		}
-		prev = tmp;
-		tmp = tmp->next;
+		prev = envar;
+		envar = envar->next;
 	}
 }
 
 void	ft_unset(t_ms *ms)
 {
+	int		i;
 	t_listd	*envar;
 
 	envar = ms->envar;
-	if (!ms->tokens->next)
+	if (!(((t_cmd *)ms->commands->data)->arguments[0]))
 	{
 		printf("unset: not enough arguments\n");
 		return ;
 	}
 	if (!envar)
 		return ;
-	while (ms->tokens->next)
-	{
-		rm_from_env(ms, envar, ms->tokens->next->data);
-	}
+	i = -1;
+	while (((t_cmd *)ms->commands->data)->arguments[++i])
+		rm_from_env(ms, ((t_cmd *)ms->commands->data)->arguments[i]);
 }
