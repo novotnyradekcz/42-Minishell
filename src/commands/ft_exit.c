a0ea1c9b81e8b495a39ed86d15559436e0fc1b82
Radek Novotný <novotnyradekcz@gmail.com>
2024-10-22 16:07:08 +0200

    Returning correct exit status on exiting minishell

--- src/commands/ft_exit.c
+++ src/commands/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: rnovotny <rnovotny@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/21 06:33:57 by lmaresov          #+#    #+#             */
-/*   Updated: 2024/10/13 11:12:21 by rnovotny         ###   ########.fr       */
+/*   Updated: 2024/10/22 15:59:59 by rnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_exit(t_ms *ms)
 {
-	char	*exit_code;
+	int	exit_code;
 
 	if (!(((t_cmd *)ms->commands->data)->arguments[0]))
 	{
@@ -26,8 +26,8 @@ void	ft_exit(t_ms *ms)
 		printf("exit: too many arguments\n");
 		return ;
 	}
-	exit_code = ((t_cmd *)ms->commands->data)->arguments[0];
+	exit_code = ft_atoi(((t_cmd *)ms->commands->data)->arguments[0]);
 	free_all(ms);
-	exit(ft_atoi(exit_code));
+	exit(exit_code);
 }
 /* if exit_code too long - undefined behaviour (same as bash) */
